5e7647da6d4279cbbe2ff98e5f90c84fa79aa105
Nworie Kingsley <obizzkingsley@gmail.com>
2021-10-04 15:29:14 +0200

    PWA stuff setup

---
+++ src/components/Qfeed.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/Qfeed.css';
+
+class Qfeed extends Component {
+  render() {
+    return (
+      <div className='QfeedContainer'>
+        <div>
+          <p className='badge badge-secondary'>Qfeed</p>
+          <h1 className=''>Hello Africa</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Qfeed;
