e5be5fbe5b62b8804d49a6387d441b96e24ed23d
Nworie Kingsley <obizzkingsley@gmail.com>
2021-11-11 17:54:24 +0100

    confirm otp page created

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,30 +1,46 @@
 import http from './httpService';
 import { apiUrl } from '../config.json';
-import jwtDecode from "jwt-decode";
+import jwtDecode from 'jwt-decode';
 
 const apiEndpoint = apiUrl + '/users/login/';
-const tokenKey = "token"
+const tokenKey = 'token';
 
 http.setJwt(getJwt());
 
-export async function login({username, password}) {
+export async function login({ username, password }) {
   let newUsername = username.toLowerCase();
-  const {data} =  await http.post(apiEndpoint, {username: newUsername, password});
+  const { data } = await http.post(apiEndpoint, {
+    username: newUsername,
+    password,
+  });
   const jwt = data.access;
 
   localStorage.setItem(tokenKey, jwt);
 }
 
+export async function confirmEmail({ confirmationCode }) {
+  const user = getCurrentUser();
+  console.log(confirmationCode, user.username);
+
+  const url = apiUrl + '/user/verifyotp/';
+  await http.post(url, {
+    confirmationCode,
+    email: user.username,
+  });
+  // const jwt = data.access;
+
+  // localStorage.setItem(tokenKey, jwt);
+}
+
 export function logout() {
-  localStorage.removeItem(tokenKey)
+  localStorage.removeItem(tokenKey);
 }
 
 export function getCurrentUser() {
   try {
-    const jwt = localStorage.getItem(tokenKey)
+    const jwt = localStorage.getItem(tokenKey);
     return jwtDecode(jwt);
-  }
-  catch (ex) {
+  } catch (ex) {
     return null;
   }
 }
@@ -32,8 +48,7 @@ export function getCurrentUser() {
 export function getJwt() {
   try {
     return localStorage.getItem(tokenKey);
-  }
-  catch (ex) {
+  } catch (ex) {
     return null;
   }
 }
@@ -43,5 +58,6 @@ export default {
   logout,
   getCurrentUser,
   getJwt,
-  tokenKey  
-}
+  tokenKey,
+  confirmEmail,
+};
