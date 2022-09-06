1c7cdc567aefa8f7d406124f0b899f390867b158
Nworie Kingsley <obizzkingsley@gmail.com>
2022-09-06 21:36:01 +0100

    Background Sync code reverted

--- src/index.js
+++ src/index.js
@@ -26,24 +26,3 @@ serviceWorkerRegistration.register();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-const main = async () => {
-    // Check if service workers are supported
-    if ('serviceWorker' in navigator) {
-      const registration = await navigator.serviceWorker.ready;
-      // Check if periodicSync is supported
-      if ('periodicSync' in registration) {
-        // Request permission
-        const status = await navigator.permissions.query({name: 'periodic-background-sync'});    
-        if (status.state === 'granted') {
-          try {
-            // Register new sync every 24 hours
-            await registration.periodicSync.register('news', {          minInterval: 24 * 60 * 60 * 1000, 
-              // 1 day        
-            });        console.log('Periodic background sync registered!');
-          } catch(e) {
-            console.error(`Periodic background sync failed:\n${e}`);
-          }
-        }
-      }
-    }}
-    main();
