f47c167cb92d267d7a8c2a978ab0584c66a9214f
Nworie Kingsley <obizzkingsley@gmail.com>
2022-09-06 15:39:20 +0100

    Test Periodic sync and closes #134

--- src/components/qfeedComponents/Question.jsx
+++ src/components/qfeedComponents/Question.jsx
@@ -109,9 +109,9 @@ const Question = (props) => {
       >
         <img
           src={question?.user.profile_pic}
-          className="w-12 h-12 rounded-full "
+          className="w-12 h-12 rounded-full bg-background2"
           style={{ objectFit: "cover" }}
-          alt={`${question?.user.firstname} ${question?.user.lastname}`}
+          alt=""
         />
       </Link>
       <section className=" p-0 w-full">
@@ -135,7 +135,7 @@ const Question = (props) => {
               src={ellipses}
               className="w-6 h-6 rounded-full m-1 "
               style={{ objectFit: "cover" }}
-              alt=""
+              alt="menu"
             />
           </div>
 
@@ -153,7 +153,7 @@ const Question = (props) => {
             className=" text-faraday-night hover:text-faraday-night"
           >
             {/* Question head */}
-            <h3 className="text-sm sm:text-lg leading-[120%] font-semibold m-0 mb-1">
+            <h3 className="text-base sm:text-lg font-semibold m-0 mb-1">
               {question?.title}
             </h3>
             {/* Question body --optional */}
@@ -259,7 +259,7 @@ const Question = (props) => {
         </div>
 
         <Link to={`/qfeed/${question.id}`} style={{ textDecoration: "none" }}>
-          <div className="comment text-base sm:text-lg font-semibold text-brand py-[14px] bg-brnd-highlight flex justify-between">
+          <div className="comment text-base sm:text-lg font-semibold text-faraday-night py-[14px] flex justify-between">
             {question.comments === 0 ? "Leave a comment" : ""}{" "}
             {question.comments === 1 ? `${question.comments} comment` : ""}{" "}
             {question.comments > 1 ? `${question.comments} comments` : ""}{" "}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+const main = async () => {
+  // const news = await fetchNews();
+  // if (news.formattedTime) {
+  //   setLastFetchedDate(news.formattedTime);
+  // }
+
+  // const articles = news.articles;
+  // if (articles && articles.length) {
+  //   const newsElement = document.getElementById('news');
+  //   if (!newsElement) return;
+
+  //   articles.forEach(article => {
+  //     newsElement.appendChild(createArticleElement(article));
+  //   });
+  // }
+  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+    
+    const registration = await navigator.serviceWorker.ready;
+    // Check if periodicSync is supported
+    if ('periodicSync' in registration) {
+      // Request permission
+      const status = await navigator.permissions.query({
+        name: 'periodic-background-sync',
+      });
+  
+      if (status.state === 'granted') {
+        try {
+          // Register new sync every 24 hours
+          await registration.periodicSync.register('news', {
+            minInterval: 24 * 60 * 60 * 1000, // 1 day
+          });
+          console.log('Periodic background sync registered!');
+        } catch(e) {
+          console.error(`Periodic background sync failed:\nx${e}`);
+        }
+      } else {
+        console.info('Periodic background sync is not granted.');
+      }
+    } else {
+      console.log('Periodic background sync is not supported.');
+    }
+  }
+
+};
+
+main();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -22,4 +71,5 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.info);
+
