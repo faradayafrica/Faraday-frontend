0852dafc32c0d3213b912428e79af165d897b43c
jomefavourite <jfjomefavourite@gmail.com>
2022-09-27 22:39:53 +0100

    implemented useInfiniteQuery

--- src/routes/Notification.jsx
+++ src/routes/Notification.jsx
@@ -8,6 +8,7 @@ import gsap from "gsap";
 
 import "../styles/notification.css";
 import caretIcon from "../images/caret.svg";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const Notification = () => {
   const apiEndpoint = process.env.REACT_APP_API_URL + `/notifications/`;
@@ -20,28 +21,14 @@ const Notification = () => {
   const [filter, setFilter] = useState("");
   const [showFilter, setShowFilter] = useState(false);
 
+  const [page, setPage] = useState(0);
+
   const el = useRef();
   const filter_modal = useRef();
   const q = gsap.utils.selector(el);
 
   const readNotifications = notifications.filter((n) => n.is_read === true);
   const unreadNotifications = notifications.filter((n) => n.is_read === false);
-
-  useEffect(() => {
-    gsap.fromTo(
-      q(".notification-item"),
-      {
-        y: +200,
-        ease: "power2.inOut",
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        stagger: 0.1,
-      }
-    );
-  }, [loading]);
 
   const markAsRead = async (notificationId) => {
     const notificationsClone = [...notifications];
@@ -102,56 +89,77 @@ const Notification = () => {
   let nextNotificationPageUrl = "";
   const notificationRequestQueue = [];
 
-  const fetchNotifications = async (urlEndpoint) => {
-    notificationRequestQueue.push(urlEndpoint);
-    try {
-      await http.get(urlEndpoint).then((resp) => {
-        setNotifications((prev) => prev.concat(...resp.data.results.data));
-        nextNotificationPageUrl = resp.data.next;
-        console.log("New data", ...resp.data.results.data);
-        setLoading(false);
-        setLoadMore(false);
-      });
-    } catch (e) {
-      console.log(e);
-      setError("Can't fetch notifications at this time. Try again later");
-    }
+  const fetchNotifications = async (pageParam) => {
+    const resp = await http.get(
+      process.env.REACT_APP_API_URL + `/notifications/?page=${pageParam}`
+    );
+
+    return resp;
   };
 
-  //we have the scroll function but haven't called it
-  const handleScroll = (e) => {
-    if (
-      nextNotificationPageUrl &&
-      document.getElementById("notification-wrapper") !== null
-    ) {
-      if (
-        e.target.documentElement.scrollTop + window.innerHeight + 200 >=
-        e.target.documentElement.scrollHeight
-      ) {
-        if (!notificationRequestQueue.includes(nextNotificationPageUrl)) {
-          // console.log("Request Q>", questionRequestQueue);
-          setLoadMore(true);
-          fetchNotifications(nextNotificationPageUrl);
-        } else {
-          console.warn("Duplicate request from notification blocked");
-        }
-      }
+  const {
+    data,
+    isSuccess,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    isLoading,
+    isError,
+    refetch,
+  } = useInfiniteQuery(
+    ["notification"],
+    ({ pageParam = 1 }) => fetchNotifications(pageParam),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages?.length + 1;
+        return lastPage?.data?.next ? nextPage : undefined;
+      },
     }
-  };
+  );
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    fetchNotifications(apiEndpoint);
-  }, []);
+    gsap.fromTo(
+      q(".notification-item"),
+      {
+        y: +200,
+        ease: "power2.inOut",
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.1,
+      }
+    );
+  }, [isSuccess]);
+
+  useEffect(() => {
+    let fetching = false;
+    const handleScroll = async (e) => {
+      const { scrollHeight, scrollTop, clientHeight } =
+        e.target.scrollingElement;
+      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.2) {
+        fetching = true;
+        if (hasNextPage) {
+          console.log(hasNextPage, "hasNextPage");
+          await fetchNextPage();
+        }
+        fetching = false;
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [fetchNextPage, hasNextPage]);
 
   return (
-    <div className="relative w-full route-wrapper" id="notification-wrapper">
-      <div className="min-h-[70px] sm:min-h-[0px] bg-transparent"> </div>
-      <div className="flex justify-between items-center m-3">
-        <h1 className="text-2xl sm:text-2xl font-bold">Notification</h1>
+    <div className='relative w-full route-wrapper' id='notification-wrapper'>
+      <div className='min-h-[70px] sm:min-h-[0px] bg-transparent'> </div>
+      <div className='flex justify-between items-center m-3'>
+        <h1 className='text-2xl sm:text-2xl font-bold'>Notification</h1>
 
-        <div className="relative">
+        <div className='relative'>
           <button
             onClick={() => {
               toggleFilterDropDown();
@@ -162,28 +170,28 @@ const Notification = () => {
                 : "border-y border-x border-faraday-night rounded-lg py-2 "
             }
           >
-            <p className="m-0 pl-3 pr-2 inline">{filter ? filter : "Filter"}</p>{" "}
-            <img className="inline px-2" src={caretIcon} alt="" />
+            <p className='m-0 pl-3 pr-2 inline'>{filter ? filter : "Filter"}</p>{" "}
+            <img className='inline px-2' src={caretIcon} alt='' />
           </button>
           {showFilter ? (
             <div
               ref={filter_modal}
-              className="p-1 border rounded-xl filter-dropdown absolute right-0 top-12 bg-white z-40 ask-shadow w-24 opacity-0"
+              className='p-1 border rounded-xl filter-dropdown absolute right-0 top-12 bg-white z-40 ask-shadow w-24 opacity-0'
             >
               <p
-                className="border-b px-3 py-2 m-0 rounded-t-lg"
+                className='border-b px-3 py-2 m-0 rounded-t-lg'
                 onClick={() => filterNotification("unread")}
               >
                 Unread{" "}
               </p>
               <p
-                className="border-b px-3 py-2 m-0"
+                className='border-b px-3 py-2 m-0'
                 onClick={() => filterNotification("read")}
               >
                 Read{" "}
               </p>
               <p
-                className=" px-3 py-2 m-0 rounded-b-lg"
+                className=' px-3 py-2 m-0 rounded-b-lg'
                 onClick={() => {
                   filterNotification("all");
                 }}
@@ -196,24 +204,40 @@ const Notification = () => {
           )}
         </div>
       </div>
-      {loading ? (
-        !error ? (
-          <NotificationLoader />
-        ) : (
-          <div className="p-3 border-brand-highlight rounded-lg border bg-background m-3 text-center">
-            <>
-              <p className="text-xs sm:text-base ">{error}</p>
-              <SecondaryButton
-                cta="Retry"
-                action={fetchNotifications(apiEndpoint)}
-              />
-            </>
-          </div>
-        )
-      ) : (
+
+      {isLoading && <NotificationLoader />}
+
+      {isError && (
+        <div className='p-3 border-brand-highlight rounded-lg border bg-background m-3 text-center'>
+          <>
+            <p className='text-xs sm:text-base '>{error}</p>
+            <SecondaryButton cta='Retry' action={refetch()} />
+          </>
+        </div>
+      )}
+
+      {
         <div ref={el}>
           <>
-            {filter === "Read"
+            {isSuccess &&
+              data?.pages?.map((page) =>
+                page?.data?.results.data.map((item) => (
+                  <NotificationItem
+                    key={item.id}
+                    id={item.id}
+                    is_read={item.is_read}
+                    notification_type={item.notification_type}
+                    message={item.content}
+                    created={item.created}
+                    markAsRead={markAsRead}
+                    // notification type content
+                    que={item.que}
+                    follow_by={item.followed_by}
+                    commentQue={item.que_with_comment}
+                  />
+                ))
+              )}
+            {/* {filter === "Read"
               ? readNotifications.map((item) => (
                   <NotificationItem
                     key={item.id}
@@ -264,42 +288,42 @@ const Notification = () => {
                     commentQue={item.que_with_comment}
                   />
                 ))
-              : ""}
+              : ""} */}
           </>
         </div>
+      }
+
+      {isFetchingNextPage && hasNextPage ? (
+        <>
+          <div className='animate-pulse border-b notification-loader-item'>
+            <div className='w-full p-3 flex  bg-white'>
+              <div className='w-6 h-6 mr-2 bg-background2'></div>
+
+              <div className='w-full'>
+                <div className='w-8 h-8 rounded-full  bg-background2'></div>
+                <div className=' h-3 rounded-xl  bg-background2 mt-2'></div>
+                <div className='w-[55px] h-3 rounded-xl  bg-background2 mt-2'></div>
+              </div>
+            </div>
+          </div>
+
+          <div className='animate-pulse border-b notification-loader-item'>
+            <div className='w-full p-3 flex  bg-white'>
+              <div className='w-6 h-6 mr-2 bg-background2'></div>
+
+              <div className='w-full'>
+                <div className='w-8 h-8 rounded-full  bg-background2'></div>
+                <div className=' h-3 rounded-xl  bg-background2 mt-2'></div>
+                <div className='w-[140px] h-3 rounded-xl  bg-background2 mt-2'></div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+
+      {!hasNextPage && data?.pages.length && (
+        <p className='text-center'>No notification to see again</p>
       )}
-
-      <>
-        {loadMore ? (
-          <>
-            <div className="animate-pulse border-b notification-loader-item">
-              <div className="w-full p-3 flex  bg-white">
-                <div className="w-6 h-6 mr-2 bg-background2"></div>
-
-                <div className="w-full">
-                  <div className="w-8 h-8 rounded-full  bg-background2"></div>
-                  <div className=" h-3 rounded-xl  bg-background2 mt-2"></div>
-                  <div className="w-[55px] h-3 rounded-xl  bg-background2 mt-2"></div>
-                </div>
-              </div>
-            </div>
-
-            <div className="animate-pulse border-b notification-loader-item">
-              <div className="w-full p-3 flex  bg-white">
-                <div className="w-6 h-6 mr-2 bg-background2"></div>
-
-                <div className="w-full">
-                  <div className="w-8 h-8 rounded-full  bg-background2"></div>
-                  <div className=" h-3 rounded-xl  bg-background2 mt-2"></div>
-                  <div className="w-[140px] h-3 rounded-xl  bg-background2 mt-2"></div>
-                </div>
-              </div>
-            </div>
-          </>
-        ) : (
-          ""
-        )}
-      </>
     </div>
   );
 };
