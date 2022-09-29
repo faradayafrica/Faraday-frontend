9d0dbc3ba937bfb72e8c65ba87fb99e738502879
Nworie Kingsley <obizzkingsley@gmail.com>
2022-09-29 17:43:55 +0100

    -Closes #136

--- src/routes/Notification.jsx
+++ src/routes/Notification.jsx
@@ -14,7 +14,6 @@ const Notification = () => {
   const apiEndpoint = process.env.REACT_APP_API_URL + `/notifications/`;
 
   const [notifications, setNotifications] = useState([]);
-  const [error, setError] = useState("");
 
   const [filter, setFilter] = useState("");
   const [showFilter, setShowFilter] = useState(false);
@@ -61,23 +60,20 @@ const Notification = () => {
   };
 
   const filterNotification = async (type) => {
-    setError("");
+    // setError("");
     toggleFilterDropDown();
 
     if (type === "read") {
       setFilter("Read");
     } else if (type === "unread") {
       setFilter("Unread");
-      console.log(filter, "Filter");
     } else {
       setFilter("All");
-      console.log(filter, "Filter");
     }
   };
 
   const fetchNotifications = async (pageParam) => {
     const resp = await http.get(apiEndpoint + `?page=${pageParam}`);
-
     return resp;
   };
 
@@ -89,6 +85,7 @@ const Notification = () => {
     isFetchingNextPage,
     isLoading,
     isError,
+    error,
     refetch,
   } = useInfiniteQuery(
     ["notification"],
@@ -101,9 +98,11 @@ const Notification = () => {
     }
   );
 
-  console.log(data);
+  // Check for the kind of error
+  console.log(error?.response.status, "Error");
 
   useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
     gsap.fromTo(
       q(".notification-item"),
       {
@@ -114,7 +113,7 @@ const Notification = () => {
       {
         y: 0,
         opacity: 1,
-        stagger: 0.1,
+        stagger: 0.04,
       }
     );
   }, [isSuccess]);
@@ -162,7 +161,7 @@ const Notification = () => {
           {showFilter ? (
             <div
               ref={filter_modal}
-              className="p-1 border rounded-xl filter-dropdown absolute right-0 top-12 bg-white z-40 ask-shadow w-24 opacity-0"
+              className="p-1 border rounded-xl filter-dropdown absolute right-0 top-12 bg-white z-40 ask-shadow w-28 opacity-0"
             >
               <p
                 className="border-b px-3 py-2 m-0 rounded-t-lg"
@@ -196,8 +195,10 @@ const Notification = () => {
       {isError && (
         <div className="p-3 border-brand-highlight rounded-lg border bg-background m-3 text-center">
           <>
-            <p className="text-xs sm:text-base ">{error}</p>
-            <SecondaryButton cta="Retry" action={refetch()} />
+            <p className="text-xs sm:text-base ">
+              Something went wrong, please try again later
+            </p>
+            <SecondaryButton cta="Retry" action={refetch} />
           </>
         </div>
       )}
@@ -281,18 +282,6 @@ const Notification = () => {
               <div className="w-full">
                 <div className="w-8 h-8 rounded-full  bg-background2"></div>
                 <div className=" h-3 rounded-xl  bg-background2 mt-2"></div>
-                <div className="w-[55px] h-3 rounded-xl  bg-background2 mt-2"></div>
-              </div>
-            </div>
-          </div>
-
-          <div className="animate-pulse border-b notification-loader-item">
-            <div className="w-full p-3 flex  bg-white">
-              <div className="w-6 h-6 mr-2 bg-background2"></div>
-
-              <div className="w-full">
-                <div className="w-8 h-8 rounded-full  bg-background2"></div>
-                <div className=" h-3 rounded-xl  bg-background2 mt-2"></div>
                 <div className="w-[140px] h-3 rounded-xl  bg-background2 mt-2"></div>
               </div>
             </div>
@@ -300,8 +289,17 @@ const Notification = () => {
         </>
       ) : null}
 
+      {isError ? <p>{error.message}</p> : ""}
+
       {!hasNextPage && data?.pages.length && (
-        <p className="text-center">No notification to see again</p>
+        <>
+          <div className="p-3 m-3 mr-1 rounded-lg border bg-background  text-center">
+            <p className="text-xs sm:text-base m-0 ">
+              No more notification to fetch
+            </p>
+          </div>
+          <div className="h-[65px] w-full sm:hidden"></div>
+        </>
       )}
     </div>
   );
