25343bdcf595a345314bf8a88119236139ae9c0b
Nworie Kingsley <obizzkingsley@gmail.com>
2022-09-29 17:19:51 +0100

    Implemented the filter with the useInfiniteQuery

--- src/routes/Notification.jsx
+++ src/routes/Notification.jsx
@@ -5,30 +5,23 @@ import SecondaryButton from "../components/styledComponents/SecondaryButton";
 import http from "../services/httpService";
 import auth from "../services/authService";
 import gsap from "gsap";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import "../styles/notification.css";
 import caretIcon from "../images/caret.svg";
-import { useInfiniteQuery } from "@tanstack/react-query";
 
 const Notification = () => {
   const apiEndpoint = process.env.REACT_APP_API_URL + `/notifications/`;
 
   const [notifications, setNotifications] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [loadMore, setLoadMore] = useState(false);
   const [error, setError] = useState("");
 
   const [filter, setFilter] = useState("");
   const [showFilter, setShowFilter] = useState(false);
 
-  const [page, setPage] = useState(0);
-
   const el = useRef();
   const filter_modal = useRef();
   const q = gsap.utils.selector(el);
-
-  const readNotifications = notifications.filter((n) => n.is_read === true);
-  const unreadNotifications = notifications.filter((n) => n.is_read === false);
 
   const markAsRead = async (notificationId) => {
     const notificationsClone = [...notifications];
@@ -69,30 +62,21 @@ const Notification = () => {
 
   const filterNotification = async (type) => {
     setError("");
-    setLoading(true);
     toggleFilterDropDown();
 
     if (type === "read") {
       setFilter("Read");
-      setLoading(false);
     } else if (type === "unread") {
       setFilter("Unread");
-      setLoading(false);
       console.log(filter, "Filter");
     } else {
       setFilter("All");
-      setLoading(false);
       console.log(filter, "Filter");
     }
   };
 
-  let nextNotificationPageUrl = "";
-  const notificationRequestQueue = [];
-
   const fetchNotifications = async (pageParam) => {
-    const resp = await http.get(
-      process.env.REACT_APP_API_URL + `/notifications/?page=${pageParam}`
-    );
+    const resp = await http.get(apiEndpoint + `?page=${pageParam}`);
 
     return resp;
   };
@@ -116,6 +100,8 @@ const Notification = () => {
       },
     }
   );
+
+  console.log(data);
 
   useEffect(() => {
     gsap.fromTo(
@@ -154,12 +140,12 @@ const Notification = () => {
   }, [fetchNextPage, hasNextPage]);
 
   return (
-    <div className='relative w-full route-wrapper' id='notification-wrapper'>
-      <div className='min-h-[70px] sm:min-h-[0px] bg-transparent'> </div>
-      <div className='flex justify-between items-center m-3'>
-        <h1 className='text-2xl sm:text-2xl font-bold'>Notification</h1>
+    <div className="relative w-full route-wrapper" id="notification-wrapper">
+      <div className="min-h-[70px] sm:min-h-[0px] bg-transparent"> </div>
+      <div className="flex justify-between items-center m-3">
+        <h1 className="text-2xl sm:text-2xl font-bold">Notification</h1>
 
-        <div className='relative'>
+        <div className="relative">
           <button
             onClick={() => {
               toggleFilterDropDown();
@@ -170,28 +156,28 @@ const Notification = () => {
                 : "border-y border-x border-faraday-night rounded-lg py-2 "
             }
           >
-            <p className='m-0 pl-3 pr-2 inline'>{filter ? filter : "Filter"}</p>{" "}
-            <img className='inline px-2' src={caretIcon} alt='' />
+            <p className="m-0 pl-3 pr-2 inline">{filter ? filter : "Filter"}</p>{" "}
+            <img className="inline px-2" src={caretIcon} alt="" />
           </button>
           {showFilter ? (
             <div
               ref={filter_modal}
-              className='p-1 border rounded-xl filter-dropdown absolute right-0 top-12 bg-white z-40 ask-shadow w-24 opacity-0'
+              className="p-1 border rounded-xl filter-dropdown absolute right-0 top-12 bg-white z-40 ask-shadow w-24 opacity-0"
             >
               <p
-                className='border-b px-3 py-2 m-0 rounded-t-lg'
+                className="border-b px-3 py-2 m-0 rounded-t-lg"
                 onClick={() => filterNotification("unread")}
               >
                 Unread{" "}
               </p>
               <p
-                className='border-b px-3 py-2 m-0'
+                className="border-b px-3 py-2 m-0"
                 onClick={() => filterNotification("read")}
               >
                 Read{" "}
               </p>
               <p
-                className=' px-3 py-2 m-0 rounded-b-lg'
+                className=" px-3 py-2 m-0 rounded-b-lg"
                 onClick={() => {
                   filterNotification("all");
                 }}
@@ -208,10 +194,10 @@ const Notification = () => {
       {isLoading && <NotificationLoader />}
 
       {isError && (
-        <div className='p-3 border-brand-highlight rounded-lg border bg-background m-3 text-center'>
+        <div className="p-3 border-brand-highlight rounded-lg border bg-background m-3 text-center">
           <>
-            <p className='text-xs sm:text-base '>{error}</p>
-            <SecondaryButton cta='Retry' action={refetch()} />
+            <p className="text-xs sm:text-base ">{error}</p>
+            <SecondaryButton cta="Retry" action={refetch()} />
           </>
         </div>
       )}
@@ -220,6 +206,7 @@ const Notification = () => {
         <div ref={el}>
           <>
             {isSuccess &&
+              (filter === "" || filter === "All") &&
               data?.pages?.map((page) =>
                 page?.data?.results.data.map((item) => (
                   <NotificationItem
@@ -237,84 +224,76 @@ const Notification = () => {
                   />
                 ))
               )}
-            {/* {filter === "Read"
-              ? readNotifications.map((item) => (
-                  <NotificationItem
-                    key={item.id}
-                    id={item.id}
-                    is_read={item.is_read}
-                    notification_type={item.notification_type}
-                    message={item.content}
-                    created={item.created}
-                    markAsRead={markAsRead}
-                    // notification type content
-                    que={item.que}
-                    follow_by={item.followed_by}
-                    commentQue={item.que_with_comment}
-                  />
-                ))
-              : ""}
 
-            {filter === "Unread"
-              ? unreadNotifications.map((item) => (
-                  <NotificationItem
-                    key={item.id}
-                    id={item.id}
-                    is_read={item.is_read}
-                    notification_type={item.notification_type}
-                    message={item.content}
-                    created={item.created}
-                    markAsRead={markAsRead}
-                    // notification type content
-                    que={item.que}
-                    follow_by={item.followed_by}
-                    commentQue={item.que_with_comment}
-                  />
-                ))
-              : ""}
-            {filter === "All" || filter === ""
-              ? notifications.map((item) => (
-                  <NotificationItem
-                    key={item.id}
-                    id={item.id}
-                    is_read={item.is_read}
-                    notification_type={item.notification_type}
-                    message={item.content}
-                    created={item.created}
-                    markAsRead={markAsRead}
-                    // notification type content
-                    que={item.que}
-                    follow_by={item.followed_by}
-                    commentQue={item.que_with_comment}
-                  />
-                ))
-              : ""} */}
+            {isSuccess &&
+              filter === "Read" &&
+              data?.pages?.map((page) =>
+                page?.data?.results.data
+                  .filter((item) => item.is_read === true)
+                  .map((item) => (
+                    <NotificationItem
+                      key={item.id}
+                      id={item.id}
+                      is_read={item.is_read}
+                      notification_type={item.notification_type}
+                      message={item.content}
+                      created={item.created}
+                      markAsRead={markAsRead}
+                      // notification type content
+                      que={item.que}
+                      follow_by={item.followed_by}
+                      commentQue={item.que_with_comment}
+                    />
+                  ))
+              )}
+
+            {isSuccess &&
+              filter === "Unread" &&
+              data?.pages?.map((page) =>
+                page?.data?.results.data
+                  .filter((item) => item.is_read === false)
+                  .map((item) => (
+                    <NotificationItem
+                      key={item.id}
+                      id={item.id}
+                      is_read={item.is_read}
+                      notification_type={item.notification_type}
+                      message={item.content}
+                      created={item.created}
+                      markAsRead={markAsRead}
+                      // notification type content
+                      que={item.que}
+                      follow_by={item.followed_by}
+                      commentQue={item.que_with_comment}
+                    />
+                  ))
+              )}
           </>
         </div>
       }
 
       {isFetchingNextPage && hasNextPage ? (
         <>
-          <div className='animate-pulse border-b notification-loader-item'>
-            <div className='w-full p-3 flex  bg-white'>
-              <div className='w-6 h-6 mr-2 bg-background2'></div>
+          <div className="animate-pulse border-b notification-loader-item">
+            <div className="w-full p-3 flex  bg-white">
+              <div className="w-6 h-6 mr-2 bg-background2"></div>
 
-              <div className='w-full'>
-                <div className='w-8 h-8 rounded-full  bg-background2'></div>
-                <div className=' h-3 rounded-xl  bg-background2 mt-2'></div>
-                <div className='w-[55px] h-3 rounded-xl  bg-background2 mt-2'></div>
+              <div className="w-full">
+                <div className="w-8 h-8 rounded-full  bg-background2"></div>
+                <div className=" h-3 rounded-xl  bg-background2 mt-2"></div>
+                <div className="w-[55px] h-3 rounded-xl  bg-background2 mt-2"></div>
               </div>
             </div>
           </div>
 
-          <div className='animate-pulse border-b notification-loader-item'>
-            <div className='w-full p-3 flex  bg-white'>
-              <div className='w-6 h-6 mr-2 bg-background2'></div>
+          <div className="animate-pulse border-b notification-loader-item">
+            <div className="w-full p-3 flex  bg-white">
+              <div className="w-6 h-6 mr-2 bg-background2"></div>
 
-              <div className='w-full'>
-                <div className='w-8 h-8 rounded-full  bg-background2'></div>
-                <div className=' h-3 rounded-xl  bg-background2 mt-2'></div>
-                <div className='w-[140px] h-3 rounded-xl  bg-background2 mt-2'></div>
+              <div className="w-full">
+                <div className="w-8 h-8 rounded-full  bg-background2"></div>
+                <div className=" h-3 rounded-xl  bg-background2 mt-2"></div>
+                <div className="w-[140px] h-3 rounded-xl  bg-background2 mt-2"></div>
               </div>
             </div>
           </div>
@@ -322,7 +301,7 @@ const Notification = () => {
       ) : null}
 
       {!hasNextPage && data?.pages.length && (
-        <p className='text-center'>No notification to see again</p>
+        <p className="text-center">No notification to see again</p>
       )}
     </div>
   );
