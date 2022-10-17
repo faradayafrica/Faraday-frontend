5f4e1df8802283f63a24ae70b3c63e963dcf504e
Nworie Kingsley <obizzkingsley@gmail.com>
2022-10-17 20:57:24 +0100

    Intercept all 401 error and refresh the Token

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import logger from './logService';
 import { toast } from 'react-toastify';
+import {refreshJwt} from "./authService"
 // import { configure } from '@testing-library/react';
 
 axios.interceptors.response.use(null, error => {
@@ -20,6 +21,13 @@ axios.interceptors.response.use(null, error => {
 const securedRequest = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 });  
+
+securedRequest.interceptors.response.use(null, error => {
+  if(error.response.status == "401") {
+  //  window.location = "/logout";
+  refreshJwt()
+  } 
+});
 
 function setJwt(jwt) {
   securedRequest.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
