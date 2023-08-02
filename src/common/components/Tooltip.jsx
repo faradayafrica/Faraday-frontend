6410f7accf2f6435b4ba268c052ff995a7382eea
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-08-03 00:48:12 +0100

    set tooltip inline

--- src/common/components/Tooltip.jsx
+++ src/common/components/Tooltip.jsx
@@ -3,11 +3,12 @@ import gsap from "gsap";
 
 const Modal = ({ visible, icon, title, message, cancel }) => {
   const modal = useRef();
+
   useEffect(() => {
     setTimeout(() => {
       gsap.fromTo(
         modal.current,
-        { y: +50, opacity: 0 },
+        { y: +50, opacity: 0.5 },
         { y: 0, opacity: 1, ease: "power2.inOut" }
       );
     }, 200);
@@ -16,12 +17,12 @@ const Modal = ({ visible, icon, title, message, cancel }) => {
   return (
     <>
       {visible && (
-        <div className="z-50  fixed top-0 left-0 w-full h-full bg-[#000000bb] flex justify-center items-center">
+        <div className="overlay">
           <div
             ref={modal}
-            className="bg-white p-12 rounded-3xl max-w-[450px] min-w-[400px] text-center opacity-0 m-12"
+            className="bg-white p-10 rounded-3xl text-center opacity-0 m-4"
           >
-            <div className="py-[5px] px-[3px]  rounded-full bg-background2 inline-flex justify-center items-center cursor-pointer">
+            <div className="py-[5px] px-[3px] rounded-full bg-background2 inline-flex justify-center items-center cursor-pointer">
               <img src={icon} className="h-7 w-7 mx-1" alt="disclaimer" />
             </div>
             <p className="text-xl font-bold mt-2 mb-1">{title}</p>
