b09bbc56681a77e7fbdfe39919ff1f6ea827035f
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-11-25 03:28:41 +0100

    reset service worker

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,8 @@
-const cacheData = "appV1";
-
+let cacheData = "appV1";
 this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
-      return cache.addAll([
+      cache.addAll([
         "/static/js/main.chunk.js",
         "/static/js/0.chunk.js",
         "/static/js/bundle.js",
@@ -12,59 +11,29 @@ this.addEventListener("install", (event) => {
         "/index.html",
         "/",
         "/users",
-        "../src/",
       ]);
     })
   );
 });
-
 this.addEventListener("fetch", (event) => {
+  // console.warn("url",event.request.url)
+
   if (!navigator.onLine) {
     if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
-      event.respondWith(
-        caches.match(event.request).then((resp) => {
-          if (resp) {
-            return resp;
-          }
-          return fetch(event.request);
-        })
-      );
-
       event.waitUntil(
         this.registration.showNotification("Internet", {
-          body: "Internet not working",
-        })
-      );
-    } else {
-      event.respondWith(
-        caches.match(event.request).then((resp) => {
-          return resp || fetch(event.request);
+          body: "internet not working",
         })
       );
     }
+    event.respondWith(
+      caches.match(event.request).then((resp) => {
+        if (resp) {
+          return resp;
+        }
+        let requestUrl = event.request.clone();
+        fetch(requestUrl);
+      })
+    );
   }
 });
-
-// // Periodic Sync
-// this.addEventListener("periodicsync", (event) => {
-//   if (event.tag === "myPeriodicSync") {
-//     event.waitUntil(doPeriodicSync());
-//   }
-// });
-
-// function doPeriodicSync() {
-//   // Perform your periodic sync logic here
-//   console.log("Periodic sync executed");
-// }
-
-// // Background Sync
-// this.addEventListener("sync", (event) => {
-//   if (event.tag === "myBackgroundSync") {
-//     event.waitUntil(doBackgroundSync());
-//   }
-// });
-
-// function doBackgroundSync() {
-//   // Perform your background sync logic here
-//   console.log("Background sync executed");
-// }
